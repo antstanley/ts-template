46d7038f1203e70fccdcd3310a23c4b4caa775d9
Ant Stanley <antstanley@gmail.com>
2023-07-12 09:02:00 +0200

    feat: Initiat commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    project: 'tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: ['standard-with-typescript', 'prettier'],
+  rules: {},
+  ignorePatterns: '.eslintrc.cjs',
+}
